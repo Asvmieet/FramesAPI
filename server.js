baecbc20c0a7e5de2380cfccfe44011898536646
Steve Morgan <commissionsbyams@gmail.com>
2026-02-02 23:14:19 -0500

    Set up server with create board endpoint. KAN-21

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.json({status: "ok", message:"Frames backend is running."})
+})
+
+app.listen(port, ()=>{
+    console.log("Frames back end is Running ");
+})
+
+// Routes
+
+app.use("/boards/create", "./boards/createBoard.js")
