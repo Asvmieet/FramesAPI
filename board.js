e8ffc2ffc6749d09bff2b175a0d65869413ccfbd
Amit Mitra <balaranjit@outlook.com>
2026-02-03 20:51:14 +0000

    Created board schema - KAN-35

---
+++ board.js
@@ -0,0 +1,22 @@
+// Schema for Boards
+
+const mongoose = require('mongoose')
+
+const boardSchema = new mongoose.Schema({
+
+board_id: {
+type: String,
+required: true,
+unique: true
+
+},
+
+perissions:[String],
+
+
+
+})
+
+const board = mongoose.model('board', boardSchema);
+
+module.exports = board;
