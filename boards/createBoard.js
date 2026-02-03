0de499201356950fa8259088ede2f224f8a12359
Steve Morgan <commissionsbyams@gmail.com>
2026-02-02 23:20:39 -0500

    KAN-7 - Added create board endpoint @ /boards/create - WIP.

--- boards/createBoard.js
+++ boards/createBoard.js
@@ -1,3 +1,10 @@
 // POST
 // Args: Board Name, Visibiity
 // Code: Be-B-C
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/", (req, res) =>{
+res.send("Create board endpoint.")
+})
