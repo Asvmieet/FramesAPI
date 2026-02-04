eb4942c2b1881fe7e6d8d9f2962c3800f42fa30b
Steve Morgan <commissionsbyams@gmail.com>
2026-02-03 23:35:32 -0500

    Updated database.js to reflect ENV variable format. KAN-22

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-// Mongo_URI should be stored in ENV.
+// MONGO_URI should be stored in ENV.
 
 const {mongoose} = require('mongoose')
 
@@ -7,12 +7,12 @@ let connected = false;
 async function connect() {
    if (connected) return
 
-   if(!process.env.MONGOOSE_URI){   
+   if(!process.env.MONGO_URI){   
     console.log("Frames - DATABASE ERROR: Unable to connect to MongoDB | URI Missing")
      return;
       }
 
-   await mongoose.connect(process.env.MONGOOSE_URI)
+   await mongoose.connect(process.env.MONGO_URI)
    connected = true
    console.log("Frames: Connected to MongoDB")
 }
