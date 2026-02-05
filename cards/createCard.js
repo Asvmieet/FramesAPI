122b67850f748d0489ae3e222f962021f8900cbc
Amit Mitra <balaranjit@outlook.com>
2026-02-05 14:12:30 +0000

    Added create card route

--- cards/createCard.js
+++ cards/createCard.js
@@ -1,3 +1,35 @@
 // POST
 // Args: Board ID, Card Name
 // Code: Be-C-C
+
+const express = require("express");
+const router = express.Router();
+const dbConnect = require("../database.js")
+const Card = require("../schema/card.js")
+const crypto = require("crypto")
+
+router.post("/", async (req, res) =>{
+  try{
+    const db = await dbConnect()
+
+    const {title, boardID} = req.body;
+
+    const card = new Card({
+        board_id: boardID,
+        title,
+    })
+
+    await card.save()
+
+    res.status(201).json({
+        ok: true,
+        card
+    })
+  }catch(err){
+    console.log(`FRAMES_ERROR - Create Card: ${err}`)
+    res.status(500).json({ok: false, error: "Failed to create card, check logs for more information."})
+  }
+
+})
+
+module.exports = router;
