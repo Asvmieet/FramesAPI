3a034ee3fc1ec3966127f06af47c9d2c4d4e352a
Steve Morgan <commissionsbyams@gmail.com>
2026-02-17 00:15:19 -0500

    removed API key

--- server.js
+++ server.js
@@ -12,30 +12,30 @@ app.listen(port, ()=>{
     console.log(`Frames back end is Running (Away at ${port} miles per second)`);
 })
 
-function apiKey(req, res, next){
-    const apiKey = req.headers['frames-api-key']
-    if(!apiKey || apiKey !== process.env.FRAMES_API_KEY){
-        return res.status(401).json({ok: false, error:"Access denied. To use the Frames API, you need a valid API key." })
-    }
-    next()
-}
+//function apiKey(req, res, next){
+  //  const apiKey = req.headers['frames-api-key']
+    //if(!apiKey || apiKey !== process.env.FRAMES_API_KEY){
+      //  return res.status(401).json({ok: false, error:"Access denied. To use the Frames API, you need a valid API key." })
+    //}
+    //next()
+//}
 
 // Routes
 
-app.use("/boards/create", apiKey, require("./boards/createBoard.js"))
-app.use("/projects/create", apiKey, require("./projects/createProject.js"))
-app.use("/card/create", apiKey, require("./cards/createCard.js"))
-app.use("/card/edit", apiKey, require("./cards/editCard.js"))
-app.use("/card/delete", apiKey, require("./cards/deleteCard.js"))
-app.use("/column/create", apiKey, require("./cards/columns/createColumn.js"))
-app.use("/column/delete", apiKey, require("./cards/columns/deleteColumn.js"))
-app.use("/column/edit", apiKey, require("./cards/columns/editColumn.js"))
-app.use("/boards/permissions", apiKey, require("./boards/editPermissions.js"))
-app.use("/interface/loadHome", apiKey, require("./cards/loading/loadBoards.js"))
+app.use("/boards/create", require("./boards/createBoard.js"))
+app.use("/projects/create", require("./projects/createProject.js"))
+app.use("/card/create", require("./cards/createCard.js"))
+app.use("/card/edit", require("./cards/editCard.js"))
+app.use("/card/delete", require("./cards/deleteCard.js"))
+app.use("/column/create", require("./cards/columns/createColumn.js"))
+app.use("/column/delete", require("./cards/columns/deleteColumn.js"))
+app.use("/column/edit", require("./cards/columns/editColumn.js"))
+app.use("/boards/permissions", require("./boards/editPermissions.js"))
+app.use("/interface/loadHome", require("./cards/loading/loadBoards.js"))
 
 
 // Auth
 
-app.use("/auth/register", apiKey, require("./auth/register.js"))
-app.use("/auth/login", apiKey, require("./auth/login.js"))
+app.use("/auth/register", require("./auth/register.js"))
+app.use("/auth/login", require("./auth/login.js"))
 
