75cc0170cfaafe3bb0e95b87c251dd7c45fa2866
Steve Morgan <commissionsbyams@gmail.com>
2026-03-02 22:10:17 -0500

    Moved back to session storage, not cookies.

--- auth/validate.js
+++ auth/validate.js
@@ -11,9 +11,7 @@ require("dotenv").config({path: "frames.env"})
 
 router.get("/", async (req, res) => {
 try{
-    const token =
-        req.headers.authorization?.split(" ")[1] ||
-        req.cookies.frames_token;
+    const token =  req.headers.authorization?.split(" ")[1] || req.cookies.frames_token;
     if (!token){
         return res.status(401).json({ok: false, message:"No token provided"})
     }
