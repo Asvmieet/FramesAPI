7be1e6e15c4af466f4af13878c03b26d52d5b1f8
Steve Morgan <commissionsbyams@gmail.com>
2026-03-28 13:57:17 -0400

    updated ratelimiting.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set("trust proxy", 1);
 
 const limit = rateLimitPkg({
     windowMs: 1 * 60 * 1000,
-    max: 15,
+    max: 50,
     standardHeaders: true,
     legacyHeaders: false,
     message: {ok: false, error: "Too many requests. Please do not abuse the system!" }
