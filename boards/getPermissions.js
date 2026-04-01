29fc27c229a307c641201d829f01fcf49d03d7e5
Steve Morgan <commissionsbyams@gmail.com>
2026-04-01 10:27:34 -0400

    Added get permissions route

---
+++ boards/getPermissions.js
@@ -0,0 +1,33 @@
+// POST
+// Args: Board ID
+// Code: Be-G-P
+
+const express = require("express");
+const router = express.Router();
+const dbConnect = require("../../database.js")
+const Board = require("../../schema/board.js")
+require("dotenv").config({path: "frames.env"})
+
+
+router.get("/", async (req, res) =>{
+  try{
+    const db = await dbConnect()
+
+
+
+let boardsWrite = await Board.find({permissionsWrite: userID})
+let boardsRead = await Board.find({permissionsRead: userID})
+
+    res.status(200).json({
+        ok: true,
+        write: boardsWrite,
+        read: boardsRead,
+    })
+  }catch(err){
+    console.log(`FRAMES_ERROR - Get Perms: ${err}`)
+    res.status(500).json({ok: false, error: "Failed to get permissions, check logs for more information."})
+  }
+
+})
+
+module.exports = router;
